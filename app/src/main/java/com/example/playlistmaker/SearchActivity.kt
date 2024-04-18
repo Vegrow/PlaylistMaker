bb23c96da83fa05e659ca276d06fd523a0952618
Alexander Litvinov <donlal@mail.ru>
2024-04-18 16:10:49 +0300

    9 спринт.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,89 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    // Зачем нужна - не понятно, если можно напрямую достать текст из textEdit
+    private var userSearchInput = ""
+    private lateinit var inputEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        setUpViews()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_USER_INPUT_KEY, userSearchInput)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputEditText.setText(savedInstanceState.getString(SEARCH_USER_INPUT_KEY, ""))
+    }
+
+    private fun setUpViews() {
+        setUpToolbar()
+        setUpSearchBar()
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setUpSearchBar() {
+        inputEditText = findViewById<EditText>(R.id.edit_text_search)
+        val clearButton = findViewById<ImageView>(R.id.image_view_clear)
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            hideKeyboard()
+            inputEditText.clearFocus()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                userSearchInput = s.toString()
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun hideKeyboard() {
+        this.currentFocus?.let { view ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private companion object {
+        const val SEARCH_USER_INPUT_KEY = "SEARCH_USER_INPUT"
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,82 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageButton
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        findViewById<ImageButton>(R.id.button_back).setOnClickListener { onBackPressed() }
+        setUpViews()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun setUpViews() {
+        setUpToolbar()
+        setUpOptions()
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setUpOptions() {
+        val clickListener = View.OnClickListener { view -> onButtonClick(view) }
+        findViewById<TextView>(R.id.text_view_share_app).setOnClickListener(clickListener)
+        findViewById<TextView>(R.id.text_view_write_to_support).setOnClickListener(clickListener)
+        findViewById<TextView>(R.id.text_view_terms_of_use).setOnClickListener(clickListener)
+    }
+
+    private fun onButtonClick(view: View) {
+        when (view.id) {
+            R.id.text_view_share_app -> shareApp()
+            R.id.text_view_write_to_support -> sendEmail()
+            R.id.text_view_terms_of_use -> openTermsOfUse()
+        }
+    }
+
+    private fun shareApp() {
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.url_practicum_android_course))
+        startActivity(Intent.createChooser(intent, null))
+    }
+
+    private fun sendEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+            .putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email_target)))
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_header))
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_body))
+        openInDefaultApp(intent)
+    }
+
+    private fun openTermsOfUse() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.terms_of_use_url)))
+        openInDefaultApp(intent)
+    }
+
+    private fun openInDefaultApp(intent: Intent) {
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                getString(R.string.no_required_app),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
